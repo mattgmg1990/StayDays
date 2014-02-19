e4e22aac16e8abc0b1deeeff6d67a05177b494dc
ktjjang01 <ktjjang01@gmail.com>
2014-02-18 23:10:18 -0800

    tab fixes

--- src/com/weigarnes/staydays/Home.java
+++ src/com/weigarnes/staydays/Home.java
@@ -16,114 +16,114 @@ import android.widget.ImageButton;
 import android.widget.Spinner;
 
 public class Home extends Activity {
-	
+
     private DataHelper mDataHelper;
-    
+
     public AlertDialog mNoDataDialog;
-    
+
     private String SAVED_GENDER_KEY = "savedGender";
 
-    
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
         initializeAgeSpinner();
-        
+
         InputStream losData = getResources().openRawResource(R.raw.los_data);
-		InputStream codeData = getResources().openRawResource(R.raw.diagnosis_desc);
-		
-		mDataHelper = new DataHelper(losData, codeData);
-		
-		initializeAutoComplete();
+        InputStream codeData = getResources().openRawResource(R.raw.diagnosis_desc);
+
+        mDataHelper = new DataHelper(losData, codeData);
+
+        initializeAutoComplete();
 
     }
-    
-    @Override
-	protected void onSaveInstanceState(Bundle outState) {
-		outState.putString(SAVED_GENDER_KEY, getSelectedGenderString());
-		super.onSaveInstanceState(outState);
-	}
 
-	@Override
-	protected void onRestoreInstanceState(Bundle savedInstanceState) {
-		restoreSelectedGender(savedInstanceState.getString(SAVED_GENDER_KEY));
-		super.onRestoreInstanceState(savedInstanceState);
-	}
-	
-	private void initializeAutoComplete(){
-    	AutoCompleteTextView diagnosisInput = (AutoCompleteTextView) findViewById(R.id.autocomplete_diagnosis);
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString(SAVED_GENDER_KEY, getSelectedGenderString());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        restoreSelectedGender(savedInstanceState.getString(SAVED_GENDER_KEY));
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    private void initializeAutoComplete(){
+        AutoCompleteTextView diagnosisInput = (AutoCompleteTextView) findViewById(R.id.autocomplete_diagnosis);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, mDataHelper.getDiagnoses());
         diagnosisInput.setAdapter(adapter);
     }
-	
-	private String getSelectedGenderString(){
-		ImageButton femaleButton = (ImageButton) findViewById(R.id.female_button);
-    	ImageButton maleButton = (ImageButton) findViewById(R.id.male_button);
-    	
-    	if(femaleButton.isSelected()){
-    		return "female";
-    	}else if(maleButton.isSelected()){
-    		return "male";
-    	} else {
-    		return "none";
-    	}
-	}
-    
+
+    private String getSelectedGenderString(){
+        ImageButton femaleButton = (ImageButton) findViewById(R.id.female_button);
+        ImageButton maleButton = (ImageButton) findViewById(R.id.male_button);
+
+        if(femaleButton.isSelected()){
+            return "female";
+        }else if(maleButton.isSelected()){
+            return "male";
+        } else {
+            return "none";
+        }
+    }
+
     // If a gender has been selected, set the background.
     private void restoreSelectedGender(String savedGender){
-    	ImageButton femaleButton = (ImageButton) findViewById(R.id.female_button);
-    	ImageButton maleButton = (ImageButton) findViewById(R.id.male_button);
-    	
-    	if(savedGender.equals("female")){
-    		femaleButton.setBackgroundResource(R.drawable.gender_button_bg);
-    		femaleButton.setSelected(true);
-    	}
-    	
-    	if(savedGender.equals("male")){
-    		maleButton.setBackgroundResource(R.drawable.gender_button_bg);
-    		maleButton.setSelected(true);
-    	}
+        ImageButton femaleButton = (ImageButton) findViewById(R.id.female_button);
+        ImageButton maleButton = (ImageButton) findViewById(R.id.male_button);
+
+        if(savedGender.equals("female")){
+            femaleButton.setBackgroundResource(R.drawable.gender_button_bg);
+            femaleButton.setSelected(true);
+        }
+
+        if(savedGender.equals("male")){
+            maleButton.setBackgroundResource(R.drawable.gender_button_bg);
+            maleButton.setSelected(true);
+        }
     }
-    
+
     // Initialize the spinner with the resource for age groups
     public void initializeAgeSpinner(){
-    	Spinner spinner = (Spinner) findViewById(R.id.age_group);
+        Spinner spinner = (Spinner) findViewById(R.id.age_group);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this, R.array.age_groups_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
     }
-    
+
    public void onGenderClicked(View v){
-	   ImageButton thisButton = (ImageButton) v;
-	   
-	   // This is the male button
-	   if((thisButton.getId() == R.id.male_button) && !thisButton.isSelected()){
-		   // Deselect the femaleButton
-		   ImageButton femaleButton = (ImageButton) findViewById(R.id.female_button);
-		   femaleButton.setSelected(false);
-		   femaleButton.setBackgroundColor(Color.TRANSPARENT);
-		   
-		   // Select the male button
-		   thisButton.setSelected(true);
-		   thisButton.setBackgroundResource(R.drawable.gender_button_bg);
-	   }
-	   
-	   // this is the female button
-	   if((thisButton.getId() == R.id.female_button) && !thisButton.isSelected()){
-		   // Deselect the maleButton
-		   ImageButton maleButton = (ImageButton) findViewById(R.id.male_button);
-		   maleButton.setSelected(false);
-		   maleButton.setBackgroundColor(Color.TRANSPARENT);
-		   
-		   // Select the female button
-		   thisButton.setSelected(true);
-		   thisButton.setBackgroundResource(R.drawable.gender_button_bg);
-	   }
+       ImageButton thisButton = (ImageButton) v;
+
+       // This is the male button
+       if((thisButton.getId() == R.id.male_button) && !thisButton.isSelected()){
+           // Deselect the femaleButton
+           ImageButton femaleButton = (ImageButton) findViewById(R.id.female_button);
+           femaleButton.setSelected(false);
+           femaleButton.setBackgroundColor(Color.TRANSPARENT);
+
+           // Select the male button
+           thisButton.setSelected(true);
+           thisButton.setBackgroundResource(R.drawable.gender_button_bg);
+       }
+
+       // this is the female button
+       if((thisButton.getId() == R.id.female_button) && !thisButton.isSelected()){
+           // Deselect the maleButton
+           ImageButton maleButton = (ImageButton) findViewById(R.id.male_button);
+           maleButton.setSelected(false);
+           maleButton.setBackgroundColor(Color.TRANSPARENT);
+
+           // Select the female button
+           thisButton.setSelected(true);
+           thisButton.setBackgroundResource(R.drawable.gender_button_bg);
+       }
    }
-   
+
 
    public void showDialog(String title, String message) {
            if( mNoDataDialog != null && mNoDataDialog.isShowing() ) return;
@@ -140,57 +140,55 @@ public class Home extends Activity {
            mNoDataDialog.show();
    }
 
-   
+
    public void onCalculateClicked(View v){
-	   String diagnosis = ((AutoCompleteTextView) findViewById(R.id.autocomplete_diagnosis)).getText().toString();
-	   String sex = "2";
-	   String ageGroup = Integer.toString(((Spinner) findViewById(R.id.age_group)).getSelectedItemPosition() + 1);
-	   
-	   // First, check that a gender has been selected.
-	   if(!findViewById(R.id.male_button).isSelected() && !findViewById(R.id.female_button).isSelected()){
-		   showDialog("Please Select A Gender", "You have not selected a gender. Please select one and try again.");
-		   return;
-	   }
-	   
-	   if(findViewById(R.id.male_button).isSelected()){
-		   sex = "1";
-	   }	   
-	   
-	   String[] result;
-	   try {
-		   result = mDataHelper.findLosByInput(diagnosis, sex, ageGroup);
-		   
-		   // Add a string representation of the sex of the patient
-		   String readableSex = "Female";
-		   if(sex.equals("1")){
-			   readableSex = "Male";
-		   }
-		   
-		   // Calculate percentage values for each category
-		   float categoryOneVal = Float.parseFloat(result[3]) * 100.0f;
-		   float categoryTwoVal = Float.parseFloat(result[4]) * 100.0f;
-		   float categoryThreeVal = Float.parseFloat(result[5]) * 100.0f;
-		   float categoryFourVal = Float.parseFloat(result[6]) * 100.0f;
-		   
-		   if((categoryOneVal == 0) && (categoryTwoVal == 0) && (categoryThreeVal == 0) && (categoryFourVal == 0)){
-			   showDialog("No Data Stored", "While this diagnosis does exist in the database, there is no length of stay data stored for the selected patient gender and age group.");
-		   } else {
-			   Intent resultIntent = new Intent(Home.this, Result.class);
-			   
-			   resultIntent.putExtra(Result.STAYLENGTH_PERCENT_ONE, String.format("%.2f", categoryOneVal));
-			   resultIntent.putExtra(Result.STAYLENGTH_PERCENT_TWO, String.format("%.2f", categoryTwoVal));
-			   resultIntent.putExtra(Result.STAYLENGTH_PERCENT_THREE, String.format("%.2f", categoryThreeVal));
-			   resultIntent.putExtra(Result.STAYLENGTH_PERCENT_FOUR, String.format("%.2f", categoryFourVal));
-			   resultIntent.putExtra(Result.DIAGNOSIS_EXTRA, diagnosis);
-			   resultIntent.putExtra(Result.SEX_EXTRA, readableSex);
-			   resultIntent.putExtra(Result.AGE_EXTRA, ((Spinner) findViewById(R.id.age_group)).getSelectedItem().toString());
-				
-			   startActivity(resultIntent);
-		   }
-	   } catch (Exception e) {
-		   showDialog("Entered Diagnosis Not Present", "The diagnosis entered was not found in the data stored. Please make sure to choose only a diagnosis from the autocomplete list that appears as you enter text. All other inputs will not be found.");
-	   }
-	   
-	   
+       String diagnosis = ((AutoCompleteTextView) findViewById(R.id.autocomplete_diagnosis)).getText().toString();
+       String sex = "2";
+       String ageGroup = Integer.toString(((Spinner) findViewById(R.id.age_group)).getSelectedItemPosition() + 1);
+
+       // First, check that a gender has been selected.
+       if(!findViewById(R.id.male_button).isSelected() && !findViewById(R.id.female_button).isSelected()){
+           showDialog("Please Select A Gender", "You have not selected a gender. Please select one and try again.");
+           return;
+       }
+
+       if(findViewById(R.id.male_button).isSelected()){
+           sex = "1";
+       }
+
+       String[] result;
+       try {
+           result = mDataHelper.findLosByInput(diagnosis, sex, ageGroup);
+
+           // Add a string representation of the sex of the patient
+           String readableSex = "Female";
+           if(sex.equals("1")){
+               readableSex = "Male";
+           }
+
+           // Calculate percentage values for each category
+           float categoryOneVal = Float.parseFloat(result[3]) * 100.0f;
+           float categoryTwoVal = Float.parseFloat(result[4]) * 100.0f;
+           float categoryThreeVal = Float.parseFloat(result[5]) * 100.0f;
+           float categoryFourVal = Float.parseFloat(result[6]) * 100.0f;
+
+           if((categoryOneVal == 0) && (categoryTwoVal == 0) && (categoryThreeVal == 0) && (categoryFourVal == 0)){
+               showDialog("No Data Stored", "While this diagnosis does exist in the database, there is no length of stay data stored for the selected patient gender and age group.");
+           } else {
+               Intent resultIntent = new Intent(Home.this, Result.class);
+
+               resultIntent.putExtra(Result.STAYLENGTH_PERCENT_ONE, String.format("%.2f", categoryOneVal));
+               resultIntent.putExtra(Result.STAYLENGTH_PERCENT_TWO, String.format("%.2f", categoryTwoVal));
+               resultIntent.putExtra(Result.STAYLENGTH_PERCENT_THREE, String.format("%.2f", categoryThreeVal));
+               resultIntent.putExtra(Result.STAYLENGTH_PERCENT_FOUR, String.format("%.2f", categoryFourVal));
+               resultIntent.putExtra(Result.DIAGNOSIS_EXTRA, diagnosis);
+               resultIntent.putExtra(Result.SEX_EXTRA, readableSex);
+               resultIntent.putExtra(Result.AGE_EXTRA, ((Spinner) findViewById(R.id.age_group)).getSelectedItem().toString());
+
+               startActivity(resultIntent);
+           }
+       } catch (Exception e) {
+           showDialog("Entered Diagnosis Not Present", "The diagnosis entered was not found in the data stored. Please make sure to choose only a diagnosis from the autocomplete list that appears as you enter text. All other inputs will not be found.");
+       }
    }
 }
--- src/com/weigarnes/staydays/Result.java
+++ src/com/weigarnes/staydays/Result.java
@@ -7,50 +7,47 @@ import android.view.View;
 import android.widget.TextView;
 
 public class Result extends Activity{
-	
-	  public static String STAYLENGTH_PERCENT_ONE = "StayLength1";
-	  public static String STAYLENGTH_PERCENT_TWO = "StayLength2";
-	  public static String STAYLENGTH_PERCENT_THREE = "StayLength3";
-	  public static String STAYLENGTH_PERCENT_FOUR = "StayLength4";
-	  
-	  public static String DIAGNOSIS_EXTRA = "Diagnosis";
-	  public static String SEX_EXTRA = "Sex";
-	  public static String AGE_EXTRA = "AgeGroup";
-	  
-	  private String percentOne = "";
-	  private String percentTwo = "";
-	  private String percentThree = "";
-	  private String percentFour = "";
-	
-	  @Override
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.layout_result);
-	        
-	        percentOne = getIntent().getExtras().getString(STAYLENGTH_PERCENT_ONE);
-	        percentTwo = getIntent().getExtras().getString(STAYLENGTH_PERCENT_TWO);
-	        percentThree = getIntent().getExtras().getString(STAYLENGTH_PERCENT_THREE);
-	        percentFour = getIntent().getExtras().getString(STAYLENGTH_PERCENT_FOUR);
-	        
-	        String diagnosis = getIntent().getExtras().getString(DIAGNOSIS_EXTRA);
-	        String sex = getIntent().getExtras().getString(SEX_EXTRA);
-	        String age = getIntent().getExtras().getString(AGE_EXTRA);
-        
-	        ((TextView) findViewById(R.id.result_diagnosis_value)).setText(diagnosis);
-	        ((TextView) findViewById(R.id.result_sex_value)).setText(sex);
-	        ((TextView) findViewById(R.id.result_age_value)).setText(age);
-	        	        
-	        ((TextView) findViewById(R.id.stay_one_percent)).setText(percentOne + "% of Patients");
-	        ((TextView) findViewById(R.id.stay_two_percent)).setText(percentTwo + "% of Patients");
-	        ((TextView) findViewById(R.id.stay_three_percent)).setText(percentThree + "% of Patients");
-	        ((TextView) findViewById(R.id.stay_four_percent)).setText(percentFour + "% of Patients");
-	    }
-	  
-	   public void onAboutClicked(View v){
-		   Intent aboutIntent = new Intent(Result.this, About.class);
-		   
-		   startActivity(aboutIntent);
-	   }
-	
-	  	
+      public static String STAYLENGTH_PERCENT_ONE = "StayLength1";
+      public static String STAYLENGTH_PERCENT_TWO = "StayLength2";
+      public static String STAYLENGTH_PERCENT_THREE = "StayLength3";
+      public static String STAYLENGTH_PERCENT_FOUR = "StayLength4";
+
+      public static String DIAGNOSIS_EXTRA = "Diagnosis";
+      public static String SEX_EXTRA = "Sex";
+      public static String AGE_EXTRA = "AgeGroup";
+
+      private String percentOne = "";
+      private String percentTwo = "";
+      private String percentThree = "";
+      private String percentFour = "";
+
+      @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.layout_result);
+
+            percentOne = getIntent().getExtras().getString(STAYLENGTH_PERCENT_ONE);
+            percentTwo = getIntent().getExtras().getString(STAYLENGTH_PERCENT_TWO);
+            percentThree = getIntent().getExtras().getString(STAYLENGTH_PERCENT_THREE);
+            percentFour = getIntent().getExtras().getString(STAYLENGTH_PERCENT_FOUR);
+
+            String diagnosis = getIntent().getExtras().getString(DIAGNOSIS_EXTRA);
+            String sex = getIntent().getExtras().getString(SEX_EXTRA);
+            String age = getIntent().getExtras().getString(AGE_EXTRA);
+
+            ((TextView) findViewById(R.id.result_diagnosis_value)).setText(diagnosis);
+            ((TextView) findViewById(R.id.result_sex_value)).setText(sex);
+            ((TextView) findViewById(R.id.result_age_value)).setText(age);
+
+            ((TextView) findViewById(R.id.stay_one_percent)).setText(percentOne + "% of Patients");
+            ((TextView) findViewById(R.id.stay_two_percent)).setText(percentTwo + "% of Patients");
+            ((TextView) findViewById(R.id.stay_three_percent)).setText(percentThree + "% of Patients");
+            ((TextView) findViewById(R.id.stay_four_percent)).setText(percentFour + "% of Patients");
+        }
+
+       public void onAboutClicked(View v){
+           Intent aboutIntent = new Intent(Result.this, About.class);
+
+           startActivity(aboutIntent);
+       }
 }
